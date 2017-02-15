cf8bf1a6dfb82868ec520d5e630b64471c566619
John Chang <johnchang9094@gmail.com>
2017-02-15 20:42:35 +0000

    Raising Exceptions

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,10 +5,21 @@ attr_reader :bike
 
 
   def release_bike
-    Bike.new
+    # In order to get the bike back from the dock,
+    # We initialized bike in the dock method to an instance bike method. @bike
+    # So now, we can get the bike from the dock and finally RELEASE IT, because we got the bike from the dock method
+  fail 'No Bikes Available' unless @bike
+  @bike
+
   end
 
   def dock(bike)
+    #We create a new instance of the bike class
+    #bike = Bike.new
+    #when we call station.dock(bike), the new instance bike gets passed as an argument
+    #we want station.release_bike to equal bike
+    #In order to do this, we need to pass this bike in its current state to the release_bike method
+    #Hence why we make the bike we created into an instance variable, so it can be accessed by released_bike method.
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,11 +6,6 @@ describe DockingStation do
   expect(subject).to respond_to :release_bike
   end
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
-
   it { is_expected.to respond_to(:dock).with(1).argument  }
 
   it { is_expected.to respond_to(:bike) }
@@ -25,4 +20,21 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq(bike)
   end
+
+  describe '#release_bike' do
+it 'releases a bike' do
+  bike = Bike.new
+  subject.dock(bike)
+  # We want to release the bike we docked.
+  expect(subject.release_bike).to eq(bike)
+  end
+end
+
+    describe '#release_bike' do
+      it 'raises an error when there are no bikes available' do
+        #Let's not dock a bike first
+        #Remember, subject = DockStation.new
+        expect { subject.release_bike }.to raise_error 'No Bikes Available'
+      end
+    end
 end
