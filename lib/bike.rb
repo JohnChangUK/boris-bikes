e1b73461f9a26354448a447329d1f7a12613919c
John Chang <johnchang9094@gmail.com>
2017-02-18 19:56:59 +0000

    Update bike.rb

--- lib/bike.rb
+++ lib/bike.rb
@@ -2,16 +2,8 @@ class Bike
 
 attr_accessor :working
 
-def initialize
-@working = true
-end
-
-def working=(working = true)
+  def initialize(working = true)
   @working = working
-end
-
-  # def working?
-  #   true
-  # end
+  end
 
 end
